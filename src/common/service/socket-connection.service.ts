defc70dbe06e0ef28e2b07c1abe46827c49a38bc
JoschuaGoetz <joschua.goetz@gmail.com>
2021-03-13 11:58:05 +0100

    Only allow one connection. Cleanup after disconnect

--- src/common/service/socket-connection.service.ts
+++ src/common/service/socket-connection.service.ts
@@ -5,13 +5,14 @@ import {Server} from 'ws';
 import {WebRTCConnectionService} from './web-rtc-connection.service';
 import {sdpTypeConverter} from '../shared/utils';
 import {WebSocketMessage} from '../shared/types';
+import {IncomingMessage} from 'http';
 
 @Injectable({
     providedIn: 'root'
 })
 export class SocketConnectionService {
 
-    private socketConnection: WebSocket[] = [];
+    private socketConnection: WebSocket | undefined;
     private socket: Server;
     private socketPort = 11653;
 
@@ -20,12 +21,19 @@ export class SocketConnectionService {
         const wss = this.electronService.remote.require('ws');
 
         this.socket = new wss.Server({port: this.socketPort}, () => {
-            console.log('server started');
+            console.log(`server started on port ${this.socketPort}`);
         });
-        this.socket.on('connection', (ws: WebSocket) => {
-            this.socketConnection.push(ws);
-            console.log(`New connection from ${ws.url}`);
+        this.socket.on('connection', (ws: WebSocket, request: IncomingMessage) => {
+            if (request.headers.from !== 'Simulation') {
+                console.log(`Invalid connection request from ${request.socket.remoteAddress}`);
+            }
 
+            if (this.socketConnection) {
+                console.log(`New valid connection request from ${request.socket.remoteAddress}. Already got one connection. Ignoring the new one`);
+            }
+
+            console.log(`New connection from ${request.socket.remoteAddress}`);
+            this.socketConnection = ws;
             ws.on('message', (messageString: string) => {
                 const potentialType = messageString.match(/"messageType":"([a-zA-Z]+)"/);
                 if (potentialType.length !== 2) {
@@ -43,9 +51,11 @@ export class SocketConnectionService {
                 }
             });
 
-        });
-        this.socket.on('listening', () => {
-            console.log('listening on ' + this.socketPort);
+            ws.on('close', (ws: WebSocket, code: number, reason: string) => {
+                console.log(`Websocket connection closed. Reason ${reason}. Code ${code}`);
+                this.socketConnection = undefined;
+            });
+
         });
 
         this.electronService.ipcRenderer.on('reload-triggered', () => {
@@ -53,13 +63,14 @@ export class SocketConnectionService {
         });
     }
 
-    public sendMessage(message: string): void {
-        this.socketConnection.forEach(ws => ws.send(message));
+    public sendMessage(message: WebSocketMessage<any>): void {
+        if (this.socketConnection) {
+            this.socketConnection.send(message);
+        }
     }
 
     private closeSocket(): void {
         this.socket.close();
-        this.socketConnection.forEach(ws => ws.close());
     }
 
 }
