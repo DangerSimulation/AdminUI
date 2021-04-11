7cd2bbbc930807147d103311705fed19f9f4a047
JoschuaGoetz <joschua.goetz@gmail.com>
2021-04-11 20:05:41 +0200

    Make the left column resizable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {SharedService} from '../common/service/shared.service';
-import {Component} from "@angular/core";
+import {Component} from '@angular/core';
+import {fromEvent, Subscription} from 'rxjs';
 
 @Component({
 	selector: 'app-root',
@@ -7,8 +8,49 @@ import {Component} from "@angular/core";
 	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+	public leftColumnFlex = 0.3;
+	private mouseMoveSubscription: Subscription;
+	private mouseUpSubscription: Subscription;
+	private mouseX = 90000;
 
 	//Import the shared service here to have all important services loaded all the time
 	constructor(private sharedService: SharedService) {
+		this.mouseUpSubscription =
+			fromEvent(document, 'mouseup')
+				.subscribe(e => {
+					if (this.mouseMoveSubscription !== undefined) {
+						this.mouseMoveSubscription.unsubscribe();
+					}
+					this.mouseX = 90000;
+				});
+	}
+
+	public startResize(): void {
+		this.mouseMoveSubscription =
+			fromEvent(document, 'mousemove')
+				.subscribe((e: MouseEvent) => {
+					if (this.mouseX === 90000) {
+						this.mouseX = e.x;
+						return;
+					}
+
+					const dir = e.x - this.mouseX;
+					this.leftColumnFlex += dir * 0.001;
+
+					this.mouseX = e.x;
+				});
+	}
+
+	public onDragStart(): boolean {
+		return false;
+	}
+
+	public onDrop(): boolean {
+		return false;
+	}
+
+	ngOnDestroy() {
+		this.mouseMoveSubscription.unsubscribe();
+		this.mouseUpSubscription.unsubscribe();
 	}
 }
